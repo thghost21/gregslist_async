5b35097c8ad9bf664ad3690a22c548ef960e5042
thghost21 <tylerhacker21@gmail.com>
2025-03-04 13:56:35 -0700

    controller, service, model setup

--- app/App.js
+++ app/App.js
@@ -1,10 +1,12 @@
 import { AuthController } from "./Auth/AuthController.js"
 import { CarsController } from "./controllers/CarsController.js"
+import { HouseController } from "./controllers/HousesController.js"
 
 class App {
   // NOTE you must name the property here on the App class as 'authController' or our auth stuff will not work
   authController = new AuthController()
   carsController = new CarsController()
+  housesController = new HouseController()
 }
 
 window['app'] = new App()
--- app/AppState.js
+++ app/AppState.js
@@ -1,5 +1,6 @@
 import { Identity } from './Auth/Identity.js'
 import { Car } from './models/Car.js'
+import { House } from './models/House.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -9,6 +10,11 @@ class ObservableAppState extends EventEmitter {
   identity = null
   /** @type {Car[]} */
   cars = []
+
+  /**
+   * @type {House}
+   */
+  houses = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
