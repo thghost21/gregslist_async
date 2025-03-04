482567d85619a8f5559b4caca496ec9f8f2a150c
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-04 09:30:04 -0700

    removed example stuff

--- app/App.js
+++ app/App.js
@@ -1,8 +1,6 @@
-import { ExampleController } from './controllers/ExampleController.js';
 
 class App {
 
-  ExampleController = new ExampleController() // ☑️ you can remove this - example only
 
 }
 
