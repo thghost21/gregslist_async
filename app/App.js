00fec22fec055a5949986c78b5a7760525e954fd
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-04 11:37:20 -0700

    added authController to App

--- app/App.js
+++ app/App.js
@@ -1,7 +1,9 @@
+import { AuthController } from "./Auth/AuthController.js"
 import { CarsController } from "./controllers/CarsController.js"
 
 class App {
-
+  // NOTE you must name the property here on the App class as 'authController' or our auth stuff will not work
+  authController = new AuthController()
   carsController = new CarsController()
 }
 
